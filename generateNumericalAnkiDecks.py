f592d9df3afe87975e0cdd51687a03fe74ace46b
Perry Hartono <perryox@hotmail.com>
2021-11-13 13:02:07 +1100

    Make if-else statement more consistent

--- generateNumericalAnkiDecks.py
+++ generateNumericalAnkiDecks.py
@@ -43,13 +43,13 @@ def generateTens(xx, x):
 	# Edge case for ten
 	if (xx == 1 and x == 0):
 		return oneTen
+	# 1#
+	elif (tens[xx].count('{') == 1):
+		return tens[xx].format(ones[x])
 	# 2# or more
-	elif (tens[xx].count('{') == 2):
+	else:
 		one = ones[x] if x != 0 else ''
 		return tens[xx].format(ones[xx], one)
-	# 1#
-	else:
-		return tens[xx].format(ones[x])
 
 def generateHundreds(xxx, xx, x):
 	# Edge case for non-hundreds
@@ -64,14 +64,9 @@ def generateHundreds(xxx, xx, x):
 		hundred = ones[xxx] if xxx != 0 else ''
 		ten = generateTens(xx, x) if not (xx == 0 and x == 0) else ''
 		return hundreds[xxx].format(hundred, ten)
-		
-
-
 
 for xxx in range(0, 10):
 	for xx in range(0, 10):
 		for x in range(0, 10):
 			result = generateHundreds(xxx, xx, x)
 			print('{}{}{}: {}'.format(xxx, xx, x, result))
-
-
