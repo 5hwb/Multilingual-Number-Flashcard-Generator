fb6813054825dfe79c79733d1e2dce024c13f490
Perry Hartono <perryox@hotmail.com>
2021-11-13 18:28:08 +1100

    Rename 'x', 'xx' etc into 'i', 'ii' to make it more in line with Roman numerals

--- generateNumericalAnkiDecksFinnish.py
+++ generateNumericalAnkiDecksFinnish.py
@@ -11,41 +11,41 @@ ones = [
 	'yhdeksän' # 9
 ]
 
-def get1(x, isNoZero=False):
-	if isNoZero and x == 0:
+def get1(i, isNoZero=False):
+	if isNoZero and i == 0:
 		return ''
-	return ones[x]
+	return ones[i]
 
-def get10(xx, x, isNoZero=False):
-	if xx == 0:
-		return get1(x, isNoZero)
-	elif xx == 1 and x == 0:
+def get10(ii, i, isNoZero=False):
+	if ii == 0:
+		return get1(i, isNoZero)
+	elif ii == 1 and i == 0:
 		return 'kymmenen'
-	elif xx == 1:
-		return get1(x, True) + 'toista'
+	elif ii == 1:
+		return get1(i, True) + 'toista'
 	else:
-		return get1(xx, True) + 'kymmentä' + get1(x, True)
+		return get1(ii, True) + 'kymmentä' + get1(i, True)
 
-def get100(xxx, xx, x, isNoZero=False):
-	if xxx == 0:
-		return get10(xx, x, isNoZero)
-	elif xxx == 1:
-		return 'sata' + get10(xx, x, True)
+def get100(iii, ii, i, isNoZero=False):
+	if iii == 0:
+		return get10(ii, i, isNoZero)
+	elif iii == 1:
+		return 'sata' + get10(ii, i, True)
 	else:
-		return get1(xxx, True) + 'sataa' + get10(xx, x, True)
+		return get1(iii, True) + 'sataa' + get10(ii, i, True)
 
-def get1000(iv, xxx, xx, x, isNoZero=False):
+def get1000(iv, iii, ii, i, isNoZero=False):
 	if iv == 0:
-		return get100(xxx, xx, x, isNoZero)
+		return get100(iii, ii, i, isNoZero)
 	elif iv == 1:
-		return 'tuhat' + get100(xxx, xx, x, True)
+		return 'tuhat' + get100(iii, ii, i, True)
 	else:
-		return get1(iv, True) + 'tuhatta' + get100(xxx, xx, x, True)
+		return get1(iv, True) + 'tuhatta' + get100(iii, ii, i, True)
 
 # Generate numbers up to 9999
 for iv in range(10):
-	for xxx in range(10):
-		for xx in range(10):
-			for x in range(10):
-				result = get1000(iv, xxx, xx, x)
-				print('{}\t{}{}{}{}'.format(result, iv, xxx, xx, x))
+	for iii in range(10):
+		for ii in range(10):
+			for i in range(10):
+				result = get1000(iv, iii, ii, i)
+				print('{}\t{}{}{}{}'.format(result, iv, iii, ii, i))
