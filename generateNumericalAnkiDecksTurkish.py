055ac97c6065a04dd00bfb5d571216b4da170f4f
Perry Hartono <perryox@hotmail.com>
2021-11-13 21:42:05 +1100

    Add Turkish numeral generator script and number list to 9999

---
+++ generateNumericalAnkiDecksTurkish.py
@@ -0,0 +1,66 @@
+ones = [
+	'sıfır', # 0
+	'bir', # 1
+	'iki', # 2
+	'üç', # 3
+	'dört', # 4
+	'beş', # 5
+	'altı', # 6
+	'yedi', # 7
+	'sekiz', # 8
+	'dokuz' # 9
+]
+
+tens = [
+	'', # 0
+	'on', # 1
+	'yirmi', # 2
+	'otuz', # 3
+	'kırk', # 4
+	'elli', # 5
+	'altmış', # 6
+	'yetmiş', # 7
+	'seksen', # 8
+	'doksan' # 9
+]
+
+oneHundred = 'yüz'
+
+oneThousand = 'bin'
+
+def get1(i, isNoZero=False):
+	if isNoZero and i == 0:
+		return ''
+	return ones[i]
+
+def get10(ii, i, isNoZero=False):
+	if ii == 0:
+		return get1(i, isNoZero)
+	elif i == 0:
+		return tens[ii]
+	else:
+		return tens[ii] + ' ' + get1(i, True)
+
+def get100(iii, ii, i, isNoZero=False):
+	if iii == 0:
+		return get10(ii, i, isNoZero)
+	elif iii == 1:
+		return oneHundred + ' ' + get10(ii, i, True)
+	else:
+		return get1(iii, True) + ' ' + oneHundred + ' ' + get10(ii, i, True)
+
+def get1000(iv, iii, ii, i, isNoZero=False):
+	if iv == 0:
+		return get100(iii, ii, i, isNoZero)
+	elif iv == 1:
+		return oneThousand + ' ' + get100(iii, ii, i, True)
+	else:
+		return get1(iv, True) + ' ' + oneThousand + ' ' + get100(iii, ii, i, True)
+
+# Generate numbers up to 9999
+for iv in range(10):
+	for iii in range(10):
+		for ii in range(10):
+			for i in range(10):
+				result = get1000(iv, iii, ii, i)
+				print('{}\t{}{}{}{}'.format(result, iv, iii, ii, i))
