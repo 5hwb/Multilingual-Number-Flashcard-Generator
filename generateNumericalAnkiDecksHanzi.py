cbc7a48b18ab3239f30d753cb6a73b4956bed50e
Perry Hartono <perryox@hotmail.com>
2021-11-14 01:07:56 +1100

    Renamed files, added Chinese num gen script (both hanzi and Mandarin pronunciation) and Anki import list

--- generateNumericalAnkiDecksHanzi.py
+++ generateNumericalAnkiDecksHanzi.py
@@ -1,23 +1,23 @@
 ones = [
-	'líng', # 0
-	'yī', # 1
-	'èr', # 2
-	'sān', # 3
-	'sì', # 4
-	'wǔ', # 5
-	'liù', # 6
-	'qī', # 7
-	'bā', # 8
-	'jiǔ' # 9
+	'零', # 0
+	'一', # 1
+	'二', # 2
+	'三', # 3
+	'四', # 4
+	'五', # 5
+	'六', # 6
+	'七', # 7
+	'八', # 8
+	'九' # 9
 ]
 
-twoAlt = 'liǎng'
+twoAlt = '两'
 
-oneTen = 'shí'
+oneTen = '十'
 
-oneHundred = 'bǎi'
+oneHundred = '百'
 
-oneThousand = 'qiān'
+oneThousand = '千'
 
 def get1(i, isNoZero=False):
 	if isNoZero and i == 0:
@@ -28,25 +28,25 @@ def get10(ii, i, isNoZero=False):
 	if ii == 0:
 		return get1(i, isNoZero)
 	elif ii == 1:
-		return oneTen + ' ' + get1(i, True)
+		return oneTen + get1(i, True)
 	else:
-		return get1(ii, True) + ' ' + oneTen + ' ' + get1(i, True)
+		return get1(ii, True) + oneTen + get1(i, True)
 
 def get100(iii, ii, i, isNoZero=False):
 	if iii == 0:
 		return get10(ii, i, isNoZero)
 	elif iii == 2:
-		return twoAlt + ' ' + oneHundred + ' ' + get10(ii, i, True)
+		return twoAlt + oneHundred + get10(ii, i, True)
 	else:
-		return get1(iii, True) + ' ' + oneHundred + ' ' + get10(ii, i, True)
+		return get1(iii, True) + oneHundred + get10(ii, i, True)
 
 def get1000(iv, iii, ii, i, isNoZero=False):
 	if iv == 0:
 		return get100(iii, ii, i, isNoZero)
 	elif iv == 2:
-		return twoAlt + ' ' + oneThousand + ' ' + get100(iii, ii, i, True)
+		return twoAlt + oneThousand + get100(iii, ii, i, True)
 	else:
-		return get1(iv, True) + ' ' + oneThousand + ' ' + get100(iii, ii, i, True)
+		return get1(iv, True) + oneThousand + get100(iii, ii, i, True)
 
 # Generate numbers up to 9999
 for x in range(10000):
