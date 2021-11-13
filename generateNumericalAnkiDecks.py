fa90e70c4760bfa5067c45570f2955082d23ca77
Perry Hartono <perryox@hotmail.com>
2021-11-13 12:37:00 +1100

    Expand Finnish numeral generation to 999

--- generateNumericalAnkiDecks.py
+++ generateNumericalAnkiDecks.py
@@ -24,20 +24,58 @@ tens = [
 	'{}kymmentä{}' # 9#
 ]
 
+hundreds = [
+	'{}', # 0##
+	'sata{}', # 1##
+	'{}sataa{}', # 2##
+	'{}sataa{}', # 3##
+	'{}sataa{}', # 4##
+	'{}sataa{}', # 5##
+	'{}sataa{}', # 6##
+	'{}sataa{}', # 7##
+	'{}sataa{}', # 8##
+	'{}sataa{}', # 9##
+]
+
 oneTen = 'kymmenen'
 oneHundred = 'sata'
+oneThousand = 'tuhat'
 
-for xx in range(0, 10):
-	for x in range(0, 10):
-		# Edge case for ten
-		if (xx == 1 and x == 0):
-			result = oneTen
-			print('{}{}: {}'.format(xx, x, result))
-		elif (tens[xx].count('{') == 2):
-			one = ones[x] if x != 0 else ''
-			result = tens[xx].format(ones[xx], one)
-			print('{}{}: {}'.format(xx, x, result))
-		else:
-			result = tens[xx].format(ones[x])
-			print('{}{}: {}'.format(xx, x, result))
+def generateTens(xx, x):
+	# Edge case for ten
+	if (xx == 1 and x == 0):
+		return oneTen
+	# 2# or more
+	elif (tens[xx].count('{') == 2):
+		one = ones[x] if x != 0 else ''
+		return tens[xx].format(ones[xx], one)
+	# 1#
+	else:
+		return tens[xx].format(ones[x])
+
+def generateHundreds(xxx, xx, x):
+	# Edge case for non-hundreds
+	if (xxx == 0):
+		return generateTens(xx, x)
+	# Edge case for hundred
+	elif (xxx == 1 and xx == 0 and x == 0):
+		return oneHundred
+	# 1##
+	elif (xxx == 1):
+		return oneHundred + generateTens(xx, x)
+	# 2## or more
+	else:
+		hundred = ones[xxx] if xxx != 0 else ''
+		ten = generateTens(xx, x) if not (xx == 0 and x == 0) else ''
+		return hundreds[xxx].format(hundred, ten)
+		
+
+
+
+for xxx in range(0, 10):
+	for xx in range(0, 10):
+		for x in range(0, 10):
+			result = generateHundreds(xxx, xx, x)
+			print('{}{}{}: {}'.format(xxx, xx, x, result))
+
 
