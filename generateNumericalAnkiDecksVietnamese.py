071fda705d257f23d71b2454618c5128da6673ad
Perry Hartono <perryox@hotmail.com>
2021-11-16 00:42:44 +1100

    Fix the missing 'zero hundred' (không trăm) problem for Vietnamese, update lists to latest 0-20 and 21-9999 standard

--- generateNumericalAnkiDecksVietnamese.py
+++ generateNumericalAnkiDecksVietnamese.py
@@ -1,6 +1,15 @@
 from file_io import load_file_to_str, save_str_to_file
 
 # NOTE: WIP. need to figure out how to deal with 1001 = 'một nghìn không trăm linh một' (expected) instead of 'một nghìn một' (actual).
+# 1000 = một nghìn
+# 1001 = một nghìn - không trăm - linh một
+# 1009 = một nghìn - không trăm - linh chín
+# 1010 = một nghìn - không trăm - mười
+# 1020 = một nghìn - không trăm - hai mươi
+# compare with:
+# 1109 = một nghìn - một trăm - linh chín
+# 1110 = một nghìn - một trăm - mười
+# 1120 = một nghìn - một trăm - hai mươi
 ones = [
 	'không', # 0
 	'một', # 1
@@ -42,19 +51,20 @@ def get10(ii, i, isNoZero=False):
 	else:
 		return get1(ii, True) + ' ' + oneTenAlt + ' ' + get1(i, True, isAltOne=True, isAltFive=True)
 
-def get100(iii, ii, i, isNoZero=False):
-	if iii == 0:
+def get100(iii, ii, i, isNoZero=False, hasZeroOrdinal=False):
+	if iii == 0 and not hasZeroOrdinal:
 		return get10(ii, i, isNoZero)
 	elif ii == 0 and i != 0:
-		return get1(iii, True) + ' ' + oneHundred + ' ' + zeroAlt + ' ' + get10(ii, i, True)
+		return get1(iii, not hasZeroOrdinal) + ' ' + oneHundred + ' ' + zeroAlt + ' ' + get10(ii, i, True)
 	else:
-		return get1(iii, True) + ' ' + oneHundred + ' ' + get10(ii, i, True)
+		return get1(iii, not hasZeroOrdinal) + ' ' + oneHundred + ' ' + get10(ii, i, True)
 
 def get1000(iv, iii, ii, i, isNoZero=False):
 	if iv == 0:
 		return get100(iii, ii, i, isNoZero)
 	else:
-		return get1(iv, True) + ' ' + oneThousand + ' ' + get100(iii, ii, i, True)
+		isZeroOrdinal = (iii == 0 and not (ii == 0 and i == 0))
+		return get1(iv, True) + ' ' + oneThousand + ' ' + get100(iii, ii, i, True, hasZeroOrdinal=isZeroOrdinal)
 
 # Generate numbers
 def getListOfNumerals(start, end):
